12eda2150212e4b2bd1cdaa73ee97d8cac1eeef6
Ryan Schwoerer <rschwoerer@veeco.com>
2016-07-06 20:09:31 -0500

    added ViewModelBase and BindingEx
    vmb: simple base class that will provide seamless place for future features
    bex: provides some sensible defaults for binding (I'm looking at you TextBox)

---
+++ source/CupcakeMvvM/Framework/BindingEx.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Data;
+
+namespace CupcakeMvvM.Framework
+{
+
+    public class BindingEx : Binding
+    {
+        public BindingEx()
+            : base()
+        {
+            Configure();
+        }
+
+        public BindingEx(string path)
+            : base(path)
+        {
+            Configure();
+        }
+
+        private void Configure()
+        {
+            // ValidatesOnDataErrors=True, UpdateSourceTrigger=PropertyChanged, NotifyOnValidationError=True
+            UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+            NotifyOnValidationError = true;
+            ValidatesOnDataErrors = true;
+            ValidatesOnExceptions = true;
+        }
+    }
+}
---
+++ source/CupcakeMvvM/Framework/ViewModelBase.cs
@@ -0,0 +1,28 @@
+﻿namespace CupcakeMvvM.Framework
+{
+    public class ViewModelBase : PropertyChangedBase
+    {
+        private string _DisplayName;
+
+        /// <summary>
+        /// Creates an instance of the screen.
+        /// </summary>
+        public ViewModelBase()
+        {
+            _DisplayName = GetType().FullName;
+        }
+
+        /// <summary>
+        /// Gets or Sets the Display Name
+        /// </summary>
+        public virtual string DisplayName
+        {
+            get { return _DisplayName; }
+            set
+            {
+                _DisplayName = value;
+                NotifyOfPropertyChange();
+            }
+        }
+    }
+}
